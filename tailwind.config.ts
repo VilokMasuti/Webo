5acd895a0f850ca749e9efbabaf775cb941259d5
VilokMasuti <119694262+VilokMasuti@users.noreply.github.com>
2024-11-14 11:47:55 +0530

    tailwindcss error
    tailwindcss error

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,9 +1,11 @@
+/* eslint-disable @typescript-eslint/no-require-imports */
+/* eslint-disable @typescript-eslint/no-explicit-any */
 import type { Config } from 'tailwindcss'
 const {
   default: flattenColorPalette,
+  // eslint-disable-next-line @typescript-eslint/no-require-imports
 } = require('tailwindcss/lib/util/flattenColorPalette')
 export default {
-  plugins: [addVariablesForColors],
   darkMode: ['class'],
   content: [
     './pages/**/*.{js,ts,jsx,tsx,mdx}',
@@ -11,7 +13,7 @@ export default {
     './app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
 
-  plugins: [require('tailwindcss-animate')],
+  plugins: [require('tailwindcss-animate'), addVariablesForColors],
 } satisfies Config
 
 // This plugin adds each Tailwind color as a global CSS variable, e.g. var(--gray-200).
